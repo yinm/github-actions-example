80a5bf55b91831629314976633af7a728944bdf6
yinm <yinm.cminor9@gmail.com>
2024-09-08 16:24:17 +0900

    Update code for checking behaviour

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 console.log("hello");
+
 // @ts-expect-error
-console.log(process.env.SECRET_VALUE); // It's only experiment. Don't do this in production code.
+const secretValue = process.env.SECRET_VALUE;
+console.log(secretValue); // It's only experiment. Don't do this in production code.
+
+console.log(
+  secretValue === "dangerous!!!"
+    ? "You found the secret value...!"
+    : "It is still secret!"
+);
